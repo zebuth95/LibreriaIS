8a3da7c7f687650cdce47885ce906d22aa3d9b3b
zebuth95 <jaes9511@gmail.com>
2019-11-05 09:05:20 -0500

    My 9 commit

--- libreriaIS/settings.py
+++ libreriaIS/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
 
 # Auth
 
